c495825b878ab8f8346d98b750340530f73ea077
erenaci <43528044+erenaci@users.noreply.github.com>
2020-04-17 10:41:17 +0900

    Create stringProcessing.h

---
+++ stringProcessing.h
@@ -0,0 +1,22 @@
+/**********************************************
+* Comp 15 - Fall 2019
+* Project 2 Part 1
+* Erena Inoue
+* 11/18/19
+* stringProcessing.h
+* Function declaration for stringProcessing.cpp
+***********************************************/
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+#ifndef STRINGPROCESSING_H_
+#define STRINGPROCESSING_H_
+
+string stripNonAlphaNum(string word);
+string toLower(string word);
+
+#endif
